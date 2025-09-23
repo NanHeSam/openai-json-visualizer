288d5faf2560cc00a299321dcf7033ca2d860da7
NanHeSam <samhenan@gmail.com>
2025-09-22 17:26:46 -0700

    chore: rename project to chat-visualizer and add Vercel Analytics

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { App } from "./App";
 export function AppRouter() {
   return <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
           </Routes>
+          <Analytics />
       </BrowserRouter>;
 }
